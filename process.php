e76a46f5b78f061519f8250b83afe2998eb37992
JimmyWu <37220893+Jimmywutw@users.noreply.github.com>
2018-03-30 16:39:08 +0800

    Update process.php

--- process.php
+++ process.php
@@ -2,17 +2,19 @@
 
 
 
-     if(isset($_post['submit'])){
+    if(isset($_POST['submit'])) {
 
 
-     	echo "it works";
+     	$to = "jimmy20640@gmail.com";
+		$subject = "subject";
+		$name = "name";
+		$email = "email" . "\r\n" .
+		"CC: somebodyelse@example.com";
+
+		mail($to,$subject,$name,$email);
 
 
-     }
-
-
-
-
+    }
 
 
 
